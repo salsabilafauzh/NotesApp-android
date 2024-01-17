ae68edf0aa3d0bde1f1f5b7a13e6318399b2eb83
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-01-17 20:22:39 +0700

    login and register xml

--- app/src/main/java/com/example/notesapp/SignUp.kt
+++ app/src/main/java/com/example/notesapp/SignUp.kt
@@ -3,15 +3,9 @@ package com.example.notesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
+        setContentView(R.layout.activity_sign_up)
     }
-
-
-
 }
